59b194325a89768404b1d08126ba6928e60a34bb
Allison Domenica Recalde Bravo <domenica2603@hotmail.com>
2020-07-04 19:07:00 -0500

    atributos y metodos clase Medico

--- ProgramadeTurnos/src/clases/Medico.java
+++ ProgramadeTurnos/src/clases/Medico.java
@@ -11,11 +11,67 @@ package clases;
  */
 public class Medico {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private String nombres;
+    private String apellidos;
+    private int edad;
+    private String genero;
+    private String especialidad;
+
+    public Medico(String nombres, String apellidos, int edad, String genero, String especialidad) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.edad = edad;
+        this.genero = genero;
+        this.especialidad = especialidad;
     }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Medico{" + "nombres=" + nombres + ", apellidos=" + apellidos + ", edad=" + edad + ", genero=" + genero + ", especialidad=" + especialidad + '}';
+    }
+    
+    
+    
+    
     
 }
